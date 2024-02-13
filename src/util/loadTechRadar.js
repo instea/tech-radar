53bcdee0c7b48e057fe08a493cba36461e83fe69
sodik <stanislav.miklik@gmail.com>
2024-02-13 22:33:31 +0100

    Format text a bit

--- src/util/loadTechRadar.js
+++ src/util/loadTechRadar.js
@@ -1,9 +1,19 @@
-const { google } = require('googleapis');
-const fs = require('fs');
+const { google } = require('googleapis')
+const fs = require('fs')
 
-const credentials = require('../../instea-dev-b79e01fcea4d.json');
+const credentials = require('../../instea-dev-b79e01fcea4d.json')
 
-const sheetId = '1GxM8BRk3tXSpMWNl1Rj4VL4RvEPnFzh0sqjsCcSgk5g';
+const sheetId = '1GxM8BRk3tXSpMWNl1Rj4VL4RvEPnFzh0sqjsCcSgk5g'
+
+function normalizeText(text) {
+  return text
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
+    .replace(/\n/g, '<br>')
+}
 
 async function downloadSheetContent() {
   try {
@@ -11,24 +21,24 @@ async function downloadSheetContent() {
       credentials: credentials,
       scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
     })
-    const sheetInstance = google.sheets({ version: 'v4', auth });
+    const sheetInstance = google.sheets({ version: 'v4', auth })
 
     const infoObjectFromSheet = await sheetInstance.spreadsheets.values.get({
       spreadsheetId: sheetId,
-      range: "A2:F"
-    });
+      range: 'A2:F',
+    })
 
     console.log(infoObjectFromSheet.data.values)
 
     const data = []
-    infoObjectFromSheet.data.values.forEach(blip => {
-      if(blip[0] && blip[1] && blip[2] && blip[3]) {
+    infoObjectFromSheet.data.values.forEach((blip) => {
+      if (blip[0] && blip[1] && blip[2] && blip[3]) {
         data.push({
           name: blip[0],
           ring: blip[2],
           quadrant: blip[1],
-          isNew: "TRUE",
-          description: blip[3]
+          isNew: 'TRUE',
+          description: normalizeText(blip[3]),
         })
       }
     })
@@ -38,7 +48,7 @@ async function downloadSheetContent() {
     fs.writeFileSync(outputFile, JSON.stringify(data))
     console.log(`Wrote sheet data to '${outputFile}' -> ${data.length} blips`)
   } catch (err) {
-    console.error('Error downloading sheet content:', err);
+    console.error('Error downloading sheet content:', err)
   }
 }
 
